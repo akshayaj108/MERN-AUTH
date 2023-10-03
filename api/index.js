730f83dd975feb13032ed5a0ae8d7288df9a1713
akshayaj108 <akshayjadhao806@gmail.com>
2023-10-03 17:54:14 +0530

    created a mvc folder structure in backend api controller for logic. routes for routes and models for schema structure

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
 import express from "express";
 const app = express();
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "../routes/user.route.js";
+dotenv.config();
 
+mongoose
+  .connect(process.env.MONGOURL)
+  .then(() => console.log("DB is connected"))
+  .catch((e) => console.log("DB Server Down"));
+app.use("/api/user", userRoutes);
 app.listen(3000, () => {
   console.log("SErver is started");
 });
