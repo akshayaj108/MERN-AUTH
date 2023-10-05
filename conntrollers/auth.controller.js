a22511e95086999208766bfcf52990300d85c3e1
akshayaj108 <akshayjadhao806@gmail.com>
2023-10-05 10:25:14 +0530

    created signin api with cookie jwt token security

--- conntrollers/auth.controller.js
+++ conntrollers/auth.controller.js
@@ -1,5 +1,7 @@
 import bcryptjs from "bcryptjs";
 import userModel from "../models/user.model.js";
+import { errHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -17,6 +19,28 @@ export const signup = async (req, res, next) => {
 
     const result = await doc.save();
     res.status(201).json({ message: "User Register successfully", result });
+  } catch (error) {
+    next(error);
+  }
+};
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await userModel.findOne({ email });
+    if (!validUser) return next(errHandler(404, "User Email not found"));
+    const isAuthenticate = await bcryptjs.compareSync(
+      password,
+      isValidUser.password
+    );
+    if (!isAuthenticate) return next(errHandler(401, "Wrong Credentials"));
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password, ...rest } = validUser._doc;
+    const expiryDate = new Date(Date.now() + 3600000); //1-hour
+    return res
+      .cookie("jwt toke", token, { httpOnly: true, expires: expiryDate })
+      .status(200)
+      .json({ message: "Successfully Logged In", rest });
   } catch (error) {
     next(error);
   }
